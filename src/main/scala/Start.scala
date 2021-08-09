0b9e69ebfa61278731b75ed6785dad1812cd77fe
Sammy <sammyk118@gmail.com>
2021-08-09 12:45:36 -0700

    first commit

---
+++ src/main/scala/Start.scala
@@ -0,0 +1,22 @@
+import org.apache.spark.sql.SparkSession
+
+object Start {
+  val spark = SparkSession
+    .builder
+    .appName("hello hive")
+    .config("spark.master", "local")
+    .enableHiveSupport()
+    .getOrCreate()
+
+
+  def main(args: Array[String]): Unit = {
+    // create a spark session
+    // for Windows
+    System.setProperty("hadoop.home.dir", "C:\\winutils")
+    spark.sparkContext.setLogLevel("WARN") //reduces terminal clutter
+    println("created spark session")
+
+    println("Hello, world!")
+
+  }
+}
