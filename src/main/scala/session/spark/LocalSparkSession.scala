e07ad256a741d6b5e65f1d6bcba2c3b08ec496d4
Russell Ramirez <rrussell.public@gmail.com>
2021-08-09 22:30:09 -0500

    Separate SparkSession and create DataFrames from input files

---
+++ src/main/scala/session/spark/LocalSparkSession.scala
@@ -0,0 +1,26 @@
+package session.spark
+
+import org.apache.spark.sql.SparkSession
+
+object LocalSparkSession {
+    private var lss: SparkSession   = null
+
+    private val app_name            = "hello hive"
+    private val spark_master_set    = "spark.master"
+    private val spark_master        = "local"
+    private val hadoop_home_set     = "hadoop.home.dir"
+    private val hadoop_home         = "C:\\winutils"
+
+    def apply(): SparkSession = if (lss == null) {lss = createSession; lss} else lss
+
+    private def createSession: SparkSession = {
+        System.setProperty(hadoop_home_set, hadoop_home)
+
+        SparkSession
+        .builder
+        .appName(app_name)
+        .config(spark_master_set, spark_master)
+        .enableHiveSupport()
+        .getOrCreate()
+    }
+}
