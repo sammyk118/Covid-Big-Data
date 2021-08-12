ce4c771637d89352fba0eb8806bd84dc91aebb9f
Russell Ramirez <rrussell.public@gmail.com>
2021-08-12 16:02:05 -0500

    Fix column types

--- src/main/scala/covid/tables/DFTables.scala
+++ src/main/scala/covid/tables/DFTables.scala
@@ -2,6 +2,8 @@ package covid.tables
 
 import org.apache.spark.sql.{SparkSession, DataFrame}
 import session.spark.LocalSparkSession
+import org.apache.calcite.interpreter.Row
+import org.apache.spark.sql.DataFrameReader
 
 object DFTables {
     private val c19DataRP       = "input/covid_19_data.csv"
@@ -11,19 +13,26 @@ object DFTables {
     private val tsc19DeathsUS   = "input/time_series_covid_19_deaths_US.csv"
     private val tsc19Recovered  = "input/time_series_covid_19_recovered.csv"
 
+    private var dataCache       : DataFrame = null
+    private var confCache       : DataFrame = null
+    private var confUSCache     : DataFrame = null
+    private var deathsCache     : DataFrame = null
+    private var deathsUSCache   : DataFrame = null
+    private var recoveredCache  : DataFrame = null
+
     private val lss             = LocalSparkSession()
 
-    def getCOVID_19Data: DataFrame = lss.read.option("header", true).csv(c19DataRP)
+    def getCOVID_19Data: DataFrame = if (dataCache == null) {dataCache = createDFTable(c19DataRP).cache(); dataCache} else dataCache
 
-    def getCOVID_19Confirmed: DataFrame = lss.read.option("header", true).csv(tsc19Conf)
+    def getCOVID_19Confirmed: DataFrame = if (confCache == null) {confCache = createDFTable(tsc19Conf).cache(); confCache} else confCache
 
-    def getCOVID_19ConfirmedUS: DataFrame = lss.read.option("header", true).csv(tsc19ConfUS)
+    def getCOVID_19ConfirmedUS: DataFrame = if (confUSCache == null) {confUSCache = createDFTable(tsc19ConfUS).cache(); confUSCache} else confUSCache
 
-    def getCOVID_19Deaths: DataFrame = lss.read.option("header", true).csv(tsc19Deaths)
+    def getCOVID_19Deaths: DataFrame = if (deathsCache == null) {deathsCache = createDFTable(tsc19Deaths).cache(); deathsCache} else deathsCache
 
-    def getCOVID_19DeathsUS: DataFrame = lss.read.option("header", true).csv(tsc19DeathsUS)
+    def getCOVID_19DeathsUS: DataFrame = if (deathsUSCache == null) {deathsUSCache = createDFTable(tsc19DeathsUS).cache(); deathsUSCache} else deathsUSCache
 
-    def getCOVID_19Recovered: DataFrame = lss.read.option("header", true).csv(tsc19Recovered)
+    def getCOVID_19Recovered: DataFrame = if (recoveredCache == null) {recoveredCache = createDFTable(tsc19Recovered).cache(); recoveredCache} else recoveredCache
 
 
     // The header methods are probably not necessary, but I've included them in the off chance that they could be of convenience
@@ -39,4 +48,7 @@ object DFTables {
     def getHeaderCOVID_19DeathsUS: Array[String] = getCOVID_19DeathsUS.columns
 
     def getHeaderCOVID_19Recovered: Array[String] = getCOVID_19Recovered.columns
+    
+
+    private def createDFTable(tablePath: String): DataFrame = lss.read.options(Map("inferSchema" -> "true", "header" -> "true")).csv(tablePath)
 }
