e028087ba469927b6c9aa57906f95c5b87c2745d
masubedi37 <masubedi37@gmail.com>
2021-08-17 16:06:30 -0500

    2nd commit

--- src/main/scala/trends/T2TrendName.scala
+++ src/main/scala/trends/T2TrendName.scala
@@ -1,51 +1,67 @@
 package trends
 
-import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.{DataFrame, Row, SparkSession}
 import org.apache.spark.sql.types.{StringType, StructField, StructType}
-import org.apache.spark.sql.{Row, SparkSession}
 import org.apache.spark.sql.functions.col
 import session.spark.LocalSparkSession
 import covid.tables.DFTables
+import trends.util.DateValDiff
 
 import scala.collection.immutable.Range.Int
 
 object TrendTwo {
+  private val listofCountry = "'US', 'Canada'"
 
+  def createFiltered_country(mytable: DataFrame) : DataFrame = {
+    mytable.createOrReplaceTempView("temp")
+    val spark = LocalSparkSession()
+    val selected_column = createSum_select(mytable.columns,"temp")
+    spark.sql(s"select $selected_column from temp where `Country/Region` in ($listofCountry) group by `Country/Region` ")
+  }
+  def createSum_select (columnName: Array[String], viewName : String): String ={
+    val (ltcolumn, rtcolumn ) = sparate_header_dates(columnName)
+    val map_rtcolumn = rtcolumn.map(coll => s"sum(`$coll`)")
+    val connect = format_column_explicit(ltcolumn.drop(1).dropRight(2), viewName) ++ map_rtcolumn
+    val formatted_string = connect.mkString(", ")
+    formatted_string
+
+  }
 
   def first: Unit = {
-    // val mysession = SparkSession.builder.master("local").appName("scala").enableHiveSupport().getOrCreate()
     val spark = LocalSparkSession()
-    val mytable = DFTables.getCOVID_19Recovered
+    val mytable1 = DFTables.getCOVID_19Confirmed
+    val mytable2 = DFTables.getCOVID_19Recovered
+    val mytable3 = DFTables.getCOVID_19Deaths
     val sc = spark.sparkContext
-    val sqlContext = new org.apache.spark.sql.SQLContext(sc)
-    import sqlContext.implicits._
-    mytable.createOrReplaceTempView("recover")
-    //spark.sql ("select * from recover").show(500, 300, false)
-    spark.sql("select * from recover where Lat = 40.0")
-    val old_format = spark.sql("select * from recover where Lat = 40.0 ")
-    val dropColumn = old_format.drop("Province/State", "Country/Region", "Lat", "Long")
-    val rdd1 = dropColumn.rdd
-    //rdd1.collect.foreach(println)
-    // val rdd2 = rdd1.map(x => { var row = Seq(x);println(row)})
-    //val rdd2 = rdd1.map(x => x.toSeq.map(y => x(30).toString.toInt - x(29).toString.toInt))
-    val rdd2 = rdd1.map(x => x.toSeq.zipWithIndex.map { case (y, idx) => if (idx < x.length - 1 && x(idx + 1).toString.toInt - x(idx).toString.toInt >= 0) {
-      x(idx + 1).toString.toInt - x(idx).toString.toInt
-    }
-    else if (idx < x.length - 1) {
-      0
-    }
-    else {
-      0
-    }})
-    rdd2.collect.foreach(println)
+    sc.setLogLevel("ERROR")
 
 
-    //rdd2.collect.foreach(println)
+    val date_table1 = DateValDiff.divideDiffDF(createFiltered_country(mytable1))
+    val date_table2 =DateValDiff .divideDiffDF(createFiltered_country(mytable2))
+    val date_table3 = DateValDiff.divideDiffDF(createFiltered_country(mytable3))
 
-    //new_format.collect.foreach(println)
+    val (lfCols,rtCols) = sparate_header_dates (date_table1.columns)
+    val formatted_left_column = format_column_explicit ( lfCols, "infection1")
+    val rtcol_infection = format_column_explicit (rtCols, "infection1")
+    val rtcol_recover = format_column_explicit (rtCols, "recover1")
+    val rtcol_death = format_column_explicit (rtCols, "death1")
+    val composite_rtcol = rtcol_infection.zip(rtcol_recover.zip(rtcol_death) ).map{case (infection,(recover, death)) => s"$infection - $recover - $death"}
+    val combined_col = formatted_left_column ++ composite_rtcol
+    val selected_col = combined_col.mkString(", ")
 
-    //val revover2 = rdd1.filter.Int(x => (x(1)+x(0)))
-
+    date_table1.createOrReplaceTempView("infection1")
+    date_table2.createOrReplaceTempView("recover1")
+    date_table3.createOrReplaceTempView("death1")
+    spark.sql(s" select $selected_col from infection1 inner join recover1 on infection1.'Country/Region` = recover1.'Country/Region`  inner join death1 on infection1.'Country/Region` = death1.'Country/Region` ")
+      .show()
+  }
+  def format_column_explicit(column: Array[String], view_name: String) : Array[String] = {
+    column.map(x=> s"$view_name.`$x`")
+  }
+  def sparate_header_dates(column: Array[String]):(Array[String], Array[String])= {
+    val leftCols = column.filter(col => "\\d".r.findFirstIn(col).isEmpty)
+    val rightCols = column.filter(col => "\\d".r.findFirstIn(col).isDefined)
+    (leftCols, rightCols)
 
   }
 }
