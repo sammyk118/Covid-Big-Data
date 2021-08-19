f117179ecec62d51848f6864128fb1e6ea8be65c
Russell Ramirez <rrussell.public@gmail.com>
2021-08-19 14:04:01 -0500

    Add column rename parameter for DateValDiff

--- src/main/scala/trends/util/DateValDiff.scala
+++ src/main/scala/trends/util/DateValDiff.scala
@@ -4,11 +4,11 @@ import org.apache.spark.rdd.RDD
 import org.apache.spark.sql.{DataFrame, Row}
 
 object DateValDiff {
-    def divideDiffDF: DataFrame => DataFrame = df_handle => {
+    def divideDiffDF(df_handle: DataFrame, newCols: Seq[(String,String)] => Seq[String] = RowFunctionBase.defColsOp _): DataFrame = {
         val rc = (df_handle.rdd, df_handle.columns.toSeq)
         val diff: (Long,Long) => Long = (a,b) => a - b
 
-        CovidRowFunctions.vicinalDateOp(diff,rc)._1
+        CovidRowFunctions.vicinalDateOp(diff,rc, newCols)._1
     }
 
     def divideDiffRDD: Tuple2[RDD[Row],Seq[String]] => Tuple2[RDD[Row],Seq[String]] = rc => {
