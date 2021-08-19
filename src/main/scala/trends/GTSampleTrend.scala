181b3002535bf8ba5afd1c0fbec7085cdabb0a08
Sammy <sammyk118@gmail.com>
2021-08-19 07:09:16 -0700

    placeholder commit:

--- src/main/scala/trends/GTSampleTrend.scala
+++ src/main/scala/trends/GTSampleTrend.scala
@@ -1,13 +1,21 @@
 package trends
 import trends.util.DateValDiff
 import covid.tables.DFTables
+import trends.util.DateMax
 import session.spark.LocalSparkSession
+import org.apache.spark.sql.{DataFrame, Row, SparkSession}
 
 object T0worstDays {
   def findMax: Unit = {
+    val spark      = LocalSparkSession()
+    import spark.implicits._
+
+
     val confirmed = DFTables.getCOVID_19Confirmed
     println("hooray!")
+    val filtered = confirmed.filter(
     val conDiff = DateValDiff.divideDiffDF(confirmed)
+    conDiff.cache()
     conDiff.show()
     val cDiffNeat = conDiff.drop("Province/State", "Country/Region", "Lat", "Long")
     val cDiffNeatRDD = cDiffNeat.rdd
@@ -21,12 +29,13 @@ object T0worstDays {
 //          ele.toString.toInt
       )
     })
-
-    val zipmaxRDD = maxrdd.zipWithIndex
-//    val zipmaxDF = SparkSession.createDataFrame(zipmaxRDD)
-    zipmaxRDD.zip(x => {
-      println(x)
-    })
+    val maxDays = DateMax.findMaxIncludeCountryAndDate(conDiff)
+    maxDays.foreach(x => println(x))
+//    val maxDaysWithSchema: DataFrame = spark.createDataFrame(maxDays).toDF("country", "Date", "num")
+//    val zipmaxRDD = maxrdd.zipWithIndex
+//    zipmaxRDD.zip(x => {
+//      println(x)
+//    })
     //val country rdd =
     //need tp get country name and date alongside max val. would getting the vertical difference help find the corresponding date and country for max val in covid_19_data?
     //can i just get column name in the map?
